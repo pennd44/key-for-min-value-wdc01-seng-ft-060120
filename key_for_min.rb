41d2e242a9371812a7857bd76d1bb05efc146feb
Penn M Diba <pdiba@oxbowschool.org>
2020-05-29 17:49:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min = 10000000
+  min_key = key
+  name_hash.each do |key, value|
+    if value< min
+      value = min
+    end
+  end
+  min_key
 end
